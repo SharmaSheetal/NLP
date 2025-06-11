470b2f2672e4ffdb59cb64c8d03109562559d128
SharmaSheetal <sharmasheetal9798@gmail.com>
2025-06-11 14:23:33 -0600

    fast api

--- Projects/textSummarization/app.py
+++ Projects/textSummarization/app.py
@@ -42,4 +42,4 @@ async def predict(text: str):
         return Response(content=f"An error occurred during prediction: {str(e)}", media_type="text/plain")
     
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
